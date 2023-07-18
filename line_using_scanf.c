c73bb07892dae04dbc20f052e55448fc803763ca
Abhishek-sys-coder <60433401+Abhishek-sys-coder@users.noreply.github.com>
2023-07-19 00:36:28 +0530

    Update line_using_scanf.c

--- line_using_scanf.c
+++ line_using_scanf.c
@@ -25,19 +25,25 @@ int main()
   puts("Enter the length of your line");
   int line_length;
   scanf("%i", &line_length);
+  
   char line[line_length+2];
   puts("Make sure the length of each of your words does not exceed 30 characters");
   char word[31];
+  
   int index;
   *line='\0';
+  
   while(strlen(line)<line_length)
   {
     scanf("%30s",word);
     index=strlen(line);
     form_line(line+index, word);
   }
+  
   line[strlen(line)-1]='\0';
+  
   printf("Length of the line formed : %lu\n", strlen(line));
   printf("%s\n",line);
+  
   return 0;
 }
